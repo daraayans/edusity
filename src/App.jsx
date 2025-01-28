f8ff70fd709a4391e140ca784ee4a67350e6623d
daraayans.oluwadarasimi <daraayans@gmail.com>
2025-01-28 21:46:49 +0100

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/hero'
 import Programs from './Components/Programs/Programs'
@@ -12,7 +12,7 @@ import VideoPlayer from './Components/VideoPlayer/VideoPlayer'
 
 const App = () => {
 
-  // const [playState, setPlayState] = useState(false);
+  const [playState, setPlayState] = useState(false);
   
   return (
     <div>
@@ -21,7 +21,7 @@ const App = () => {
       <div className="container">
         <Title subTitle='Our PROGRAM' title='What we Offer' />
         <Programs/>
-        <About/>
+        <About setPlayState={setPlayState} />
         <Title subTitle='Gallery' title='Campus Photos' />
         <Campus/>
         <Title subTitle='TESTIMONIALS' title='What Student Says' />
@@ -30,7 +30,7 @@ const App = () => {
         <Contact/>
         <Footer/>
       </div>
-      {/* <VideoPlayer playState={playState}/> */}
+      <VideoPlayer playState={playState} setPlayState={setPlayState} />
     </div>
   )
 } 
